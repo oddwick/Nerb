e4c8b519c9c67dbee0d7e1067c06b950744a6dba
oddwick <oddwick@users.noreply.github.com>
2019-08-03 10:22:32 -0600

    Update header.php

--- app/public/pages/header.php
+++ app/public/pages/header.php
@@ -1,20 +1,22 @@
-<!-- begin error bar -->
 <?PHP if( $_REQUEST['msg'] ){ ?>
+<!-- begin message bar -->
 <div class="msg-bar no-margin" id="msg-bar">
 	<p>
 	<span class="material-icons md-light md-18 anchor close" data-close="msg-bar">close</span> 
 	<?= urldecode( $_REQUEST['msg'] ); ?>
     </p>
 </div>
+<!-- end message bar -->
 <?PHP } elseif( $_REQUEST['error'] ){ ?>
+<!-- begin error bar -->
 <div class="error-bar no-margin" id="error-bar">
 	<p>
 	<span class="material-icons md-light md-18 anchor close" data-close="error-bar">close</span> 
 	<?= urldecode( $_REQUEST['error'] ); ?>
      </p>
 </div>
-<?PHP } ?>
 <!-- end error bar -->	
+<?PHP } ?>
 
 
 <header>
@@ -22,10 +24,10 @@
 	<div class="grid-x">
 		<div class="cell small-10 medium-4">
 			<!--
-<a href="/">
+			<a href="/">
 				<img src="/img/logo.png" title="Nerb Application Framework" alt="Nerb Application Framework Logo" class="site-logo"/>
 			</a>
--->
+			-->
 		</div>
 	</div>
 </div>
