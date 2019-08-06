472e55236faadef544ead00162105dd6f4c7b29a
oddwick <oddwick@users.noreply.github.com>
2019-08-06 09:50:04 -0600

    Update credentials.php

--- app/config/credentials.php
+++ app/config/credentials.php
@@ -4,17 +4,10 @@
     #	this is required for connecting to the database
     #
     $DB = array(
-        "host" => "localhost",
-        "name" => "playground",
-        "user" => "test",
-        "pass" => "drowssap",
-    );
-		
-    $DB_ = array(
-        "host" => "localhost",
-        "name" => "database_name",
-        "user" => "your user name",
-        "pass" => "password",
+		"host" => "localhost",
+		"name" => "database_name",
+		"user" => "your user name",
+		"pass" => "password",
     );
 		
 ?>
