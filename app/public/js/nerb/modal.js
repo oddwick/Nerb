4c5b06ae9605afdf620f7bb803234e166a1d808b
oddwick <oddwick@users.noreply.github.com>
2019-08-09 08:36:54 -0600

    Updated

--- app/public/js/nerb/modal.js
+++ app/public/js/nerb/modal.js
@@ -1,90 +1,53 @@
 
 // pop modal window
 $(window).on('load', function () {
-		$( ".modal-window" ).click(function(event) {
-			if( $(event.target).attr('class') == 'modal-window' ) {
-				$( this ).fadeOut("fast");
-			}
-		});
+	$( ".modal-window" ).click(function(event) {
+		if( $(event.target).attr('class') == 'modal-window' ) {
+			$( this ).fadeOut("fast");
+		}
+	});
 		
 		
-		// modal trigger
-		$( "[data-modal-id]" ).click(function(event) {
-			$( "#" + $(this).attr('data-modal-id') ).fadeIn();
-			//if( $(event.target).attr('class') == 'modal-window' ) {
-				//$( this ).fadeOut();
-			//}   
-			centerModal();
-
-		});
+	// modal trigger
+	$( "[data-modal-id]" ).click(function(event) {
+		$( "#" + $(this).attr('data-modal-id') ).fadeIn();
+		//if( $(event.target).attr('class') == 'modal-window' ) {
+			//$( this ).fadeOut();
+		//}   
+		centerModal();
+	});
 		
+	$( "[data-modal-close]" ).click(function(event) {
+		if( $(this).parent().parent().attr('class') == 'modal-window' ){
+			$(this).parent().parent().fadeOut("fast");
+		} else if( $(this).attr('data-modal-close') != '' ){
+			$("#"+$(this).attr('data-modal-close') ).fadeOut("fast");
+		}
+	});
 		
-		// flyout trigger
-		$( "[data-flyout-id]" ).click(function(event) {
-			toggleFlyout( $( "#" + $(this).attr('data-flyout-id')) );
-		});
-		
-		$( ".flyout" ).click(function(event) {
-			if( $(event.target).attr('class') == 'flyout' ) {
-				toggleFlyout( this );
-			}
-		});
-		
-		
-		$( "[data-modal-close]" ).click(function(event) {
-			if( $(this).parent().parent().attr('class') == 'modal-window' ){
-				$(this).parent().parent().fadeOut("fast");
-			} else if( $(this).attr('data-modal-close') != '' ){
-				$("#"+$(this).attr('data-modal-close') ).fadeOut("fast");
-			}
-		});
-		
-		
-		$( "[data-alert-close]" ).click(function(event) {
-			if( $(this).attr('data-alert-close') != '' ){
-				$("#"+$(this).attr('data-alert-close') ).fadeOut("fast");
-			} else {
-				$(this).parent().fadeOut("fast");
-			}
-		});
+	$( "[data-alert-close]" ).click(function(event) {
+		if( $(this).attr('data-alert-close') != '' ){
+			$("#"+$(this).attr('data-alert-close') ).fadeOut("fast");
+		} else {
+			$(this).parent().fadeOut("fast");
+		}
+	});
 		
 		
  });	
-
-
-
-
-
-
-
-//data-flyout-id="stamp-list"
-
-
-
-
-function toggleFlyout( id ){
-	if( $( id ).is(":visible") ){
-		$("[data-flyout-panel]").toggle("slide", {direction:'right', speed:'fast'}, function (){
-			$( id ).fadeOut("fast");
-		});
-	} else {
-		$( id ).fadeIn("fast", function(){
-			$("[data-flyout-panel]").toggle("slide", {speed: 100, direction:'right'} );
-		});
-	}
-}
-
-
-
 
 
 $(window).resize(function(){
 	centerModal();
 });
 
-
+/**
+ * centerModal function.
+ * 
+ * @access public
+ * @return void
+ */
 function centerModal(){
-	
 	$( ".modal-content" ).position({
 	    my: "center",
 	    at: "center",
@@ -94,7 +57,6 @@ function centerModal(){
 	    }
 	});
 	/*
-	
     $('.modal-content').css({
         position:'absolute',
         left: ($(window).width() - $('.modal-content').outerWidth())/2,
@@ -103,26 +65,3 @@ function centerModal(){
     */
 	
 }
-
-
-function toggleModalDialog( target, text1, text2 ){
-	
-	currentText = $(event.target).text();
-	$(event.target).text( currentText == text1 ? text2 : text1 );
-	$('#'+target ).slideToggle( 200, function(){
-		 centerModal();
-		 });
-	//centerModal(); 
-	//alert( $(event.target).text() );
-	
-}
-
-/*
-function openModal(id){
-	$('#rating-'+id).load('/api.php?mode=likePage&id='+id);		
-}
-
-function closeModal(id){
-	$(id).hide();		
-}
-*/
